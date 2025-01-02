47a8ff5bfec93866f24103980b9aa841fca65ba5
DavidMandak <dejf.mandak@gmail.com>
2025-01-02 21:45:43 +0100

    Fix

--- Programs/Advent_of_Code_02.py
+++ Programs/Advent_of_Code_02.py
@@ -1,9 +1,8 @@
-import Intcode, time
+import Intcode
 program = list(map(int, open("../Inputs/Advent_of_Code_02.txt").read()[:-1].split(",")))
 program[1], program[2] = 12, 2
 save = program[:]
 
-s = time.time()
 Intcode.run(program)
 print(program[0])
 for noun in range(99):
@@ -13,5 +12,4 @@ for noun in range(99):
         Intcode.run(program)
         if program[0] == 19690720:
             print(100*noun+verb)
-            print(time.time()-s)
             exit()
